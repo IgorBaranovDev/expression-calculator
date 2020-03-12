437e7cbac7c06923547169486cf0891b9f7482f6
Baranov Igor <igor.82bv@mail.ru>
2020-03-12 23:02:29 +0300

    feat: final solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
+}
+
+function checkBrecketsInExpr(arr) {
+  let counter = 0;
+  for (let value of arr) {
+    if (value === '(') {
+      counter++;
+    }
+    if (value === ')') {
+      counter--;
+    }
+  }
+  if (counter !== 0)
+    throw new Error('ExpressionError: Brackets must be paired');
+}
+
+function checkDivisionByZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '/') {
+      if (arr[i + 2] === '0') throw new Error('TypeError: Division by zero.');
+    }
+  }
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  const arrExpr = expr.split('');
+  checkBrecketsInExpr(arrExpr);
+  checkDivisionByZero(arrExpr);
+  const exprWithNotSpace = expr.replace(/\s/g, '');
+  const getResult = new Function('return ' + exprWithNotSpace);
+  return getResult();
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator
+};
